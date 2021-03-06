3db2014c0322e8944363fa6a2580f0faa0f45cee
Rinisha Khadgi <itsrii.97@gmail.com>
2021-03-06 21:14:54 +0545

    added login and logout functionality

--- django_blog_project/settings.py
+++ django_blog_project/settings.py
@@ -114,4 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'blog:home'
