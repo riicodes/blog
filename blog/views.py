2c7a93433955b342f4e7d548fc9f6b4cb1eae848
Rinisha Khadgi <itsrii.97@gmail.com>
2021-03-06 19:31:06 +0545

    added templates and created base model

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Post
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    posts = Post.objects.all().order_by('-created')
+    return render(request, 'blog/home.html', {'posts': posts})
